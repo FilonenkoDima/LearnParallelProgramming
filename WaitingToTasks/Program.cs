2cde0b5d3cb4c3063a5084db649951398a64ed32
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-25 16:21:03 +0200

    Waiting for tasks

---
+++ WaitingToTasks/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace WaitingToTasks
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var cts = new CancellationTokenSource();
+			var token = cts.Token;
+			var t = new Task(() =>
+			{
+				Console.WriteLine("I take 5 seconds");
+
+				for (int i = 0; i < 5; i++)
+				{
+					token.ThrowIfCancellationRequested();
+					Thread.Sleep(1000);
+				}
+
+				Console.WriteLine("I'm done. ");
+			}, token);
+			t.Start();
+
+			Task t2 = Task.Factory.StartNew(() => Thread.Sleep(3000), token);
+
+			Task.WaitAll(new[] {t, t2}, 4000, token);
+
+			Console.WriteLine($"Task t status id {t.Status}");
+			Console.WriteLine($"Task t2 status id {t2.Status}");
+
+			Console.WriteLine("Main program done.");
+			Console.ReadKey();
+		}
+	}
+}
