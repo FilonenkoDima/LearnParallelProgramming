7242e591a33900f1484ccc9711b5d6020d2d0475
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-25 16:59:40 +0200

    Exception handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace ExceptionHandling
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Test2();
+			}
+			catch (AggregateException ae)
+			{
+				foreach (var e in ae.InnerExceptions)
+				{
+					Console.WriteLine($"Handled elsewhere: {e.GetType()}");
+				}
+			}
+			Console.WriteLine("Main program done. ");
+			Console.ReadLine();
+		}
+
+		private static void Test()
+		{
+			var t = Task.Factory.StartNew(() =>
+			{
+				throw new InvalidOperationException("Can`t do this!") { Source = "t" };
+			});
+
+			var t2 = Task.Factory.StartNew(() =>
+			{
+				throw new AccessViolationException("Can't access this") { Source = "t2" };
+			});
+
+			try
+			{
+				Task.WaitAll(t, t2);
+			}
+			catch (AggregateException ae)
+			{
+				foreach (var e in ae.InnerExceptions)
+				{
+					Console.WriteLine($"Exception {e.GetType()} from {e.Source}");
+				}
+			}
+		}
+
+		private static void Test2()
+		{
+			var t = Task.Factory.StartNew(() =>
+			{
+				throw new InvalidOperationException("Can`t do this!") { Source = "t" };
+			});
+
+			var t2 = Task.Factory.StartNew(() =>
+			{
+				throw new AccessViolationException("Can't access this") { Source = "t2" };
+			});
+
+			try
+			{
+				Task.WaitAll(t, t2);
+			}
+			catch (AggregateException ae)
+			{
+				ae.Handle(e =>
+				{
+					if (e is InvalidOperationException)
+					{
+						Console.WriteLine("Invalid operation!");
+						return true;
+					}
+					return false;
+				});
+			}
+		}
+	}
+}
