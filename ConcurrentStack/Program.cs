8fb37c14535c23d273679f7651421b8eefaed8b2
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-25 23:58:58 +0200

    ConcurrentStack

---
+++ ConcurrentStack/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConcurrentStack_
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var stack = new ConcurrentStack<int>();
+			stack.Push(1);
+			stack.Push(2);
+			stack.Push(3);
+			stack.Push(4);
+
+			int result;
+			if(stack.TryPeek(out result))
+			{
+				Console.WriteLine($"{result} is on top");
+			}
+
+			if(stack.TryPop(out result))
+			{
+				Console.WriteLine($"Popped - {result}");
+			}
+
+			var items = new int[5]; 
+			if(stack.TryPopRange(items, 0, 5) > 0)
+			{
+				var text = string.Join(", ", items.Select(x => x.ToString()));
+				Console.WriteLine("Popped this items: " + text);
+			}
+		}
+	}
+}
