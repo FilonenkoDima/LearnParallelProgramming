435d612b8a399baf92dcd7f2a53fac0e4c083001
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-25 23:42:31 +0200

    ConcurrentQueue

---
+++ ConcurrentQueue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConcurrentQueue_
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var q = new ConcurrentQueue<int>();
+			q.Enqueue(1);
+			q.Enqueue(2);
+
+			// 2 1 <- front
+
+			int result;
+			if (q.TryDequeue(out result))
+			{
+				Console.WriteLine($"Remove element {result}");
+			}
+
+			if(q.TryPeek(out result))
+			{
+				Console.WriteLine($"Front element is {result}");
+			}
+		}
+	}
+}
