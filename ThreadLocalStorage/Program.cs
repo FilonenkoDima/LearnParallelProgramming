ab84785f0c56d34f6274cc99d724669feeebf8bd
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-30 22:14:14 +0200

    Thread Local Storage

---
+++ ThreadLocalStorage/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ThreadLocalStorage
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			object o = new object();
+			int sum = 0;
+			lock (o)
+			{
+				Parallel.For(1, 1001, x =>
+				{
+					sum += x;
+				});
+				Console.WriteLine(sum);
+			}
+
+			lock (o)
+			{
+				sum = 0;
+				Parallel.For(1, 1001, x =>
+				{
+					Interlocked.Add(ref sum, x);
+				});
+				Console.WriteLine(sum);
+			}
+
+			lock (o)
+			{
+				sum = 0;
+
+				Parallel.For(1, 1001,
+					() => 0,
+					(x, state, tls) =>
+					{
+						tls += x;
+					Console.WriteLine($"Task {Task.CurrentId} has sum {tls}");
+						return tls;
+					},
+					partialSum =>
+					{
+						Console.WriteLine($"Partial value of task {Task.CurrentId} is {partialSum}");
+						Interlocked.Add(ref sum, partialSum);
+					});
+
+				Console.WriteLine(sum);
+			}
+		}
+	}
+}
