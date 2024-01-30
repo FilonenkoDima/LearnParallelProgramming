41ae9821976422d746f547bc633abaf68eb3d915
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-30 23:40:17 +0200

    Merge Options

---
+++ MergeOptions/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MergeOptions
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var numbers = Enumerable.Range(1, 20).ToArray();
+
+			var results = numbers
+				.AsParallel()
+				.WithMergeOptions(ParallelMergeOptions.FullyBuffered)
+				.Select(x =>
+			{
+				var result = Math.Log10(x);
+				Console.Write($"P {result}\t");
+				return result;
+			});
+
+			foreach( var result in results )
+			{
+				Console.Write($"C {result}\t");
+			}
+		}
+	}
+}
