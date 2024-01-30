e06f05cd3f0bb681a64e981e9a00ce2e7f7b1b96
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-30 23:19:57 +0200

    AsParallel and ParallelQuery

---
+++ AsParallelAndParallelQuery/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AsParallelAndParallelQuery
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const int count = 50;
+
+			var items = Enumerable.Range(0, count).ToArray();
+			var results = new int[count];
+
+			items.AsParallel().ForAll(x =>
+			{
+				int newValue = x * x * x;
+				Console.WriteLine($"{newValue} ({Task.CurrentId})");
+				results[x] = newValue;
+			});
+			Console.WriteLine();
+			Console.WriteLine();
+
+			foreach (var item in results)
+			{
+				Console.WriteLine(item);
+			}
+				Console.WriteLine();
+				Console.WriteLine();
+
+			var cubes = items.AsParallel().AsOrdered().Select(x => x*x*x);
+
+			foreach (var item in cubes)
+				Console.WriteLine(item);
+		}
+	}
+}
