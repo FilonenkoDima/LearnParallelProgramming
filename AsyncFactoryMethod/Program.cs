92280b42bf243069fce2713c119daaa091cf9b2c
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-31 15:44:30 +0200

    Async Factory Method

---
+++ AsyncFactoryMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace AsyncFactoryMethod
+{
+	public class Foo
+	{
+		private Foo()
+		{
+			// 
+		}
+
+		private async Task<Foo> InitAsync()
+		{
+			await Task.Delay(1000);
+			return this;
+		}
+
+		public static Task<Foo> CreateAsync()
+		{
+			var result = new Foo();
+			return result.InitAsync();
+		}
+	}
+	internal class Program
+	{
+		static async Task Main(string[] args)
+		{
+			//var foo = new Foo();
+			//await foo.InitAsync();
+
+			Foo x = await Foo.CreateAsync();
+
+		}
+	}
+}
